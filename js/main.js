989abfe856ce31475d323704448543fc8834486c
ramdomf <77049930+ramdomf@users.noreply.github.com>
2021-01-06 11:30:25 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,136 @@
+const btn = document.querySelector('button');
+
+
+
+
+document.addEventListener("click", function() {
+    let num = Math.random() * 42;
+    num = Math.ceil(num);
+    if (num == '1') {
+        alert('Киев');
+    }
+    else if (num == '2') {
+        alert('Харьков');
+    }
+    else if (num == '3') {
+        alert('Одесса');
+    }
+    else if (num == '4') {
+        alert('Днепр');
+    }
+    else if (num == '5') {
+        alert('Донецк');
+    }
+    else if (num == '6') {
+        alert('	Запорожье');
+    }
+    else if (num == '7') {
+        alert('	Львов');
+    }
+    else if (num == '8') {
+        alert('Кривой Рог');
+    }
+    else if (num == '9') {
+        alert('Николаев');
+    }
+    else if (num == '10') {
+        alert('Севастополь');
+    } 
+    else if (num == '11') {
+        alert('	Мариуполь');
+    }
+    else if (num == '12') {
+        alert('Луганск');
+    }
+    else if (num == '13') {
+        alert('Винница');
+    }
+    else if (num == '14') {
+        alert('Симферополь');
+    }
+    else if (num == '15') {
+        alert('	Макеевка');
+    }
+    else if (num == '16') {
+        alert('		Херсон');
+    }
+    else if (num == '17') {
+        alert('		Чернигов');
+    }
+    else if (num == '18') {
+        alert('Полтава');
+    }
+    else if (num == '19') {
+        alert('	Черкассы');
+    }
+    else if (num == '20') {
+        alert('Хмельницкий');
+    }
+    else if (num == '21') {
+        alert('Черновцы');
+    }
+    else if (num == '22') {
+        alert('Житомир');
+    }
+    else if (num == '23') {
+        alert('Сумы');
+    }
+    else if (num == '24') {
+        alert('Ровно');
+    }
+    else if (num == '25') {
+        alert('Горловка');
+    }
+    else if (num == '26') {
+        alert('	Ивано-Франковск');
+    }
+    else if (num == '27') {
+        alert('	Каменское');
+    }
+    else if (num == '28') {
+        alert('Кропивницкий');
+    }
+    else if (num == '29') {
+        alert('Тернополь');
+    }
+    else if (num == '30') {
+        alert('Кременчуг');
+    } 
+    else if (num == '31') {
+        alert('Луцк');
+    }
+    else if (num == '32') {
+        alert('Белая Церковь');
+    }
+    else if (num == '33') {
+        alert('Краматорск');
+    }
+    else if (num == '34') {
+        alert('Мелитополь');
+    }
+    else if (num == '35') {
+        alert('Керчь');
+    }
+    else if (num == '36') {
+        alert('Ужгород');
+    }
+    else if (num == '37') {
+        alert('Бердянск');
+    }
+    else if (num == '38') {
+        alert('Никополь');
+    }
+    else if (num == '39') {
+        alert('Славянск');
+    }
+    else if (num == '40') {
+        alert('Алчевск');
+    }
+    else if (num == '41') {
+        alert('Павлоград');
+    }
+    else if (num == '42') {
+        alert('Северодонецк');
+    }
+});
+
